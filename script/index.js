8378d33fdfc2915649036845f2b7fac48e052513
sim yoonji <124511094+simyoonji@users.noreply.github.com>
2025-10-16 18:44:51 +0900

    5

--- script/index.js
+++ script/index.js
@@ -1,112 +1,120 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// main 
-gsap.timeline({
+// -------------------------
+// 01. MAIN SECTION
+// -------------------------
+const mainTl = gsap.timeline({
   scrollTrigger: {
     trigger: "#main",
     start: "30% top",
     end: "bottom center",
     scrub: 1,
   }
-})
-.fromTo(".main__text-inner", { yPercent: 0 }, { yPercent: 100, ease: "none" });
+});
 
-
-// intro scrolltrigger
-gsap.fromTo('.intro__img-wrap > img', 
-  { scale: 1.4 }, 
-  { scale: 1,
-
-    scrollTrigger: {
-      trigger: "#intro",
-      start: "center bottom",
-      end: "200% bottom",
-      scrub: 1
-    }
-  }
-);
-
-// work scrolltrigger
- gsap.fromTo('.work-intro-swiper img', 
-   { scale: 1.4 }, 
-   { scale: 1,
-
-     scrollTrigger: {
-       trigger: ".work__intro",
-       start: "20% bottom",
-       end: "200% bottom",
-       scrub: 1
-     }
-   }
- );
-
-gsap.fromTo('.work-deco', 
-  {rotate : 360}, 
-  {rotate:0,
-    
-    scrollTrigger: {
-      trigger: ".work__intro",
-      start: "20% bottom",
-      end: "200% bottom",
-      scrub: 1
-    }
-  }
+mainTl.fromTo(".main__text-inner", 
+  { yPercent: 0 }, 
+  { yPercent: 100, ease: "none" }
 );
 
 
-// contact scrolltrigger
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: "#contact",
-      start: "top bottom",
-      end: "50% bottom",
-      scrub: 2,
-    },
-  })
-  .fromTo(".contact__text__ani-01", { x: 400, opacity : 0}, { x: 0, opacity : 1 })
-  .fromTo(".contact__text__ani-02", { x: -400, opacity : 0}, { x: 0, opacity : 1 }, "<");
+// -------------------------
+// 02. INTRO SECTION
+// -------------------------
+const introTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#intro",
+    start: "center bottom",
+    end: "200% bottom",
+    scrub: 1,
+  }
+});
+
+introTl.fromTo(".intro__img-wrap > img",
+  { scale: 1.4 },
+  { scale: 1, ease: "none" }
+);
 
 
- 
+// -------------------------
+// 03. WORK SECTION
+// -------------------------
+const workTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".work__intro",
+    start: "20% bottom",
+    end: "200% bottom",
+    scrub: 1,
+  }
+});
+
+workTl
+  .fromTo(".work-intro-swiper img", 
+    { scale: 1.4 }, 
+    { scale: 1, ease: "none" }
+  )
+  .fromTo(".work-deco", 
+    { rotate: 360 }, 
+    { rotate: 0, ease: "none" },
+    "<" // 동시에 실행
+  );
+
+
+// -------------------------
+// 04. CONTACT SECTION
+// -------------------------
+const contactTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#contact",
+    start: "top bottom",
+    end: "50% bottom",
+    scrub: 1,
+  }
+});
+
+contactTl
+  .fromTo(".contact__text__ani-01", 
+    { x: 400, opacity: 0 }, 
+    { x: 0, opacity: 1, ease: "power2.out" }
+  )
+  .fromTo(".contact__text__ani-02", 
+    { x: -400, opacity: 0 }, 
+    { x: 0, opacity: 1, ease: "power2.out" },
+    "<" // 동시에 실행
+  );
+
+
+// -------------------------
+// 05. SWIPERS
+// -------------------------
+
 // work intro swiper
- const swiperIntro = new Swiper(".work-intro-swiper", {
-   direction: "horizontal",
-   loop: true,
-   slidesPerView: 1, 
-   effect: "fade", 
-   fadeEffect: {
-     crossFade: true 
-   },
-   autoplay: {
-     delay: 1000,
-   },
- });
+const swiperIntro = new Swiper(".work-intro-swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  effect: "fade",
+  fadeEffect: { crossFade: true },
+  autoplay: {
+    delay: 1000,
+  },
+});
 
 // web design swiper
 const swiper1 = new Swiper(".swiper__01", {
   direction: "horizontal",
   loop: false,
   spaceBetween: 10,
-   autoplay: {
+  autoplay: {
     delay: 4000,
     disableOnInteraction: false,
-    pauseOnMouseEnter: true, 
+    pauseOnMouseEnter: true,
   },
-
   breakpoints: {
-    0: {
-      slidesPerView: 1, 
-    },
-    481: {
-      slidesPerView: 1,
-    },
-    769: {
-      slidesPerView: 2,
-    },
-    1200: { 
-      slidesPerView: 3,
-    },
+    0: { slidesPerView: 1 },
+    481: { slidesPerView: 1 },
+    769: { slidesPerView: 2 },
+    1200: { slidesPerView: 3 },
   },
 });
 
@@ -115,25 +123,16 @@ const swiper2 = new Swiper(".swiper__02", {
   direction: "horizontal",
   loop: false,
   spaceBetween: 10,
-   autoplay: {
+  autoplay: {
     delay: 4000,
     disableOnInteraction: false,
-    pauseOnMouseEnter: true, 
+    pauseOnMouseEnter: true,
   },
-
   breakpoints: {
-     0: {
-      slidesPerView: 1, 
-    },
-    481: {
-      slidesPerView: 1,
-    },
-    769: {
-      slidesPerView: 2,
-    },
-    1400: {
-      slidesPerView: 2,
-    },
+    0: { slidesPerView: 1 },
+    481: { slidesPerView: 1 },
+    769: { slidesPerView: 2 },
+    1400: { slidesPerView: 2 },
   },
 });
 
@@ -142,29 +141,16 @@ const swiper3 = new Swiper(".swiper__03", {
   direction: "horizontal",
   loop: true,
   spaceBetween: 10,
-   autoplay: {
+  autoplay: {
     delay: 4000,
     disableOnInteraction: false,
-    pauseOnMouseEnter: true, 
+    pauseOnMouseEnter: true,
   },
-  
   breakpoints: {
-    0: {
-      slidesPerView: 1, 
-    },
-    481: {
-      slidesPerView: 1,
-    },
-    769: {
-      slidesPerView: 2,
-    },
-    1025: {
-      slidesPerView: 3,
-    },
-    1400: {
-      slidesPerView: 4,
-    },
+    0: { slidesPerView: 1 },
+    481: { slidesPerView: 1 },
+    769: { slidesPerView: 2 },
+    1025: { slidesPerView: 3 },
+    1400: { slidesPerView: 4 },
   },
 });
-
-
